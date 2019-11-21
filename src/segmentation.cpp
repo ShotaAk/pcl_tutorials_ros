497f619630dc85f37c07fe8a1245a319163551fb
ShotaAk <macakasit@gmail.com>
2019-11-21 15:25:10 +0900

    Add euclidean cluster extraction

--- src/segmentation.cpp
+++ src/segmentation.cpp
@@ -1,4 +1,5 @@
 #include <ros/ros.h>
+#include <vector>
 #include <sensor_msgs/PointCloud2.h>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_ros/transform_broadcaster.h>
@@ -11,6 +12,12 @@
 #include <pcl/sample_consensus/method_types.h>
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
+
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/segmentation/extract_clusters.h>
 
 
 static ros::Publisher PubOutput;
@@ -78,6 +85,118 @@ void plane(const sensor_msgs::PointCloud2ConstPtr& cloud_msg, const std::string 
     PubOutput.publish(output);
 }
 
+void euclideanClusterExtraction(const sensor_msgs::PointCloud2ConstPtr& cloud_msg, const std::string frame_id){
+    // Euclidean Cluster Extraction
+    // Ref: http://www.pointclouds.org/documentation/tutorials/cluster_extraction.php#cluster-extraction
+    
+    enum COLOR_RGB{
+        RED=0,
+        GREEN,
+        BLUE,
+        COLOR_MAX
+    };
+    const int CLUSTER_MAX = 10;
+    const int CLUSTER_COLOR[CLUSTER_MAX][COLOR_MAX] = {
+        {230, 0, 18},{243, 152, 18}, {255, 251, 0},
+        {143, 195, 31},{0, 153, 68}, {0, 158, 150},
+        {0, 160, 233},{0, 104, 183}, {29, 32, 136},
+        {146, 7, 131}
+    };
+
+    // Convert the sensor_msgs/PointCloud2 data to pcl/PointCloud
+    // Use pcl::PointXYZRGB to visualize segmentation.
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>()); 
+    pcl::fromROSMsg(*cloud_msg, *cloud);
+
+    // Create the filtering object: downsample the dataset using a leaf size of 1cm
+    pcl::VoxelGrid<pcl::PointXYZRGB> vg;
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZRGB>());
+    vg.setInputCloud (cloud);
+    vg.setLeafSize (0.01, 0.01, 0.01);
+    vg.filter (*cloud_filtered);
+
+    // Create the segmentation object for the planar model and set all the parameters
+    pcl::SACSegmentation<pcl::PointXYZRGB> seg;
+    pcl::PointIndices::Ptr inliers (new pcl::PointIndices());
+    pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients());
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_plane (new pcl::PointCloud<pcl::PointXYZRGB> ());
+    seg.setOptimizeCoefficients (true);
+    seg.setModelType (pcl::SACMODEL_PLANE);
+    seg.setMethodType (pcl::SAC_RANSAC);
+    seg.setMaxIterations (100);
+    seg.setDistanceThreshold (0.02);
+
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_extracted(new pcl::PointCloud<pcl::PointXYZRGB>()); 
+    int i=0, nr_points = (int) cloud_filtered->points.size ();
+    while (cloud_filtered->points.size () > 0.3 * nr_points)
+    {
+        // Segment the largest planar component from the remaining cloud
+        seg.setInputCloud (cloud_filtered);
+        seg.segment (*inliers, *coefficients);
+        if (inliers->indices.size () == 0)
+        {
+            ROS_WARN("Could not estimate a planar model for the given dataset.");
+            break;
+        }
+
+        // Extract the planar inliers from the input cloud
+        pcl::ExtractIndices<pcl::PointXYZRGB> extract;
+        extract.setInputCloud (cloud_filtered);
+        extract.setIndices (inliers);
+
+        // Remove the planar inliers, extract the rest
+        extract.setNegative (true);
+        extract.filter (*cloud_extracted);
+        *cloud_filtered = *cloud_extracted;
+    }
+
+    // Creating the KdTree object for the search method of the extraction
+    pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGB>());
+    tree->setInputCloud (cloud_filtered);
+
+    std::vector<pcl::PointIndices> cluster_indices;
+    pcl::EuclideanClusterExtraction<pcl::PointXYZRGB> ec;
+    ec.setClusterTolerance (0.02); // 2cm
+    ec.setMinClusterSize (100);
+    ec.setMaxClusterSize (25000);
+    ec.setSearchMethod (tree);
+    ec.setInputCloud (cloud_filtered);
+    ec.extract (cluster_indices);
+
+    int cluster_i=0;
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_output (new pcl::PointCloud<pcl::PointXYZRGB>());
+    for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); 
+            it != cluster_indices.end (); ++it)
+    {
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZRGB>());
+        for (std::vector<int>::const_iterator pit = it->indices.begin (); 
+                pit != it->indices.end (); ++pit){
+            cloud_filtered->points[*pit].r = CLUSTER_COLOR[cluster_i][RED];
+            cloud_filtered->points[*pit].g = CLUSTER_COLOR[cluster_i][GREEN];
+            cloud_filtered->points[*pit].b = CLUSTER_COLOR[cluster_i][BLUE];
+            cloud_cluster->points.push_back (cloud_filtered->points[*pit]); //*
+        }
+        cloud_cluster->width = cloud_cluster->points.size ();
+        cloud_cluster->height = 1;
+        cloud_cluster->is_dense = true;
+        
+        *cloud_output += *cloud_cluster;
+
+        cluster_i++;
+        if(cluster_i >= CLUSTER_MAX){
+            break;
+        }
+    }
+
+    // Convert to ROS data type
+    sensor_msgs::PointCloud2 output;
+    pcl::toROSMsg(*cloud_output, output);
+    // pcl_conversions::moveFromPCL(cloud_extracted, output);
+    output.header.frame_id = frame_id;
+    // Publish the data
+    PubOutput.publish(output);
+}
+
 
 void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 {
@@ -86,6 +205,9 @@ void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
     switch(ExampleNumber){
     case 0:
         plane(cloud_msg, EXAMPLE_FRAME_ID);
+        break;
+    case 1:
+        euclideanClusterExtraction(cloud_msg, EXAMPLE_FRAME_ID);
         break;
     default:
         break;
