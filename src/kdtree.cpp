6e313f6fda22b1c7144554df5386e810fe3eff1f
ShotaAk <macakasit@gmail.com>
2022-10-02 17:53:50 +0900

    Fix code format to pass roslint check

--- src/kdtree.cpp
+++ src/kdtree.cpp
@@ -1,8 +1,29 @@
+/*
+ * Copyright 2019 Shota Aoki
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <geometry_msgs/TransformStamped.h>
 #include <ros/ros.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <string>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_ros/transform_broadcaster.h>
-#include <geometry_msgs/TransformStamped.h>
+#include <vector>
+
 // PCL specific includes
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
@@ -14,7 +35,8 @@
 static ros::Publisher PubOutput;
 static int ExampleNumber;
 
-void tf_broadcast(const std::string frame_id){
+void tf_broadcast(const std::string frame_id)
+{
     static tf2_ros::TransformBroadcaster br;
     geometry_msgs::TransformStamped transformStamped;
     transformStamped.header.stamp = ros::Time::now();
@@ -32,8 +54,8 @@ void tf_broadcast(const std::string frame_id){
     br.sendTransform(transformStamped);
 }
 
-
-void kdTree(const sensor_msgs::PointCloud2ConstPtr& cloud_msg, const std::string frame_id){
+void kdTree(const sensor_msgs::PointCloud2ConstPtr& cloud_msg, const std::string frame_id)
+{
     // How to use a KdTree to search
     // Ref: http://pointclouds.org/documentation/tutorials/kdtree_search.php#kdtree-search
 
@@ -43,12 +65,12 @@ void kdTree(const sensor_msgs::PointCloud2ConstPtr& cloud_msg, const std::string
     pcl::fromROSMsg(*cloud_msg, cloud);
 
     pcl::KdTreeFLANN<pcl::PointXYZRGB> kdtree;
-    kdtree.setInputCloud (cloud.makeShared());
+    kdtree.setInputCloud(cloud.makeShared());
 
     pcl::PointXYZRGB searchPoint;
-    searchPoint.x = 0; // meter
-    searchPoint.y = 0; // meter
-    searchPoint.z = 0.5; // meter
+    searchPoint.x = 0;  // meter
+    searchPoint.y = 0;  // meter
+    searchPoint.z = 0.5;  // meter
 
     // K nearest neighbor search
     /*
@@ -69,14 +91,14 @@ void kdTree(const sensor_msgs::PointCloud2ConstPtr& cloud_msg, const std::string
     // Neighbors within radius search
     std::vector<int> pointIdxRadiusSearch;
     std::vector<float> pointRadiusSquaredDistance;
-    float radius = 0.1; // meter
+    float radius = 0.1;  // meter
     if ( kdtree.radiusSearch (searchPoint, radius, pointIdxRadiusSearch, pointRadiusSquaredDistance) > 0 )
     {
-        for (size_t i = 0; i < pointIdxRadiusSearch.size (); ++i){
+        for (size_t i = 0; i < pointIdxRadiusSearch.size (); ++i)
+        {
             cloud.points[ pointIdxRadiusSearch[i] ].r = 255;
             cloud.points[ pointIdxRadiusSearch[i] ].g = 0;
             cloud.points[ pointIdxRadiusSearch[i] ].b = 0;
-
         }
     }
 
@@ -88,11 +110,12 @@ void kdTree(const sensor_msgs::PointCloud2ConstPtr& cloud_msg, const std::string
     PubOutput.publish(output);
 }
 
-void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
+void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 {
-    const static std::string EXAMPLE_FRAME_ID = "example_frame";
+    const std::string EXAMPLE_FRAME_ID = "example_frame";
 
-    switch(ExampleNumber){
+    switch (ExampleNumber)
+    {
     case 0:
         kdTree(cloud_msg, EXAMPLE_FRAME_ID);
         break;
@@ -104,10 +127,10 @@ void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
     tf_broadcast(EXAMPLE_FRAME_ID);
 }
 
-int main (int argc, char** argv)
+int main(int argc, char** argv)
 {
     // Initialize ROS
-    ros::init (argc, argv, "example_kdtree");
+    ros::init(argc, argv, "example_kdtree");
     ros::NodeHandle nh("~");
 
     nh.param<int>("number", ExampleNumber, 0);
@@ -118,5 +141,5 @@ int main (int argc, char** argv)
     // Create a ROS publisher for the output point cloud
     PubOutput = nh.advertise<sensor_msgs::PointCloud2> ("/output", 1);
 
-    ros::spin ();
+    ros::spin();
 }
--- src/segmentation.cpp
+++ src/segmentation.cpp
@@ -1,9 +1,29 @@
+/*
+ * Copyright 2019 Shota Aoki
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include <geometry_msgs/TransformStamped.h>
 #include <ros/ros.h>
-#include <vector>
 #include <sensor_msgs/PointCloud2.h>
+#include <string>
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_ros/transform_broadcaster.h>
-#include <geometry_msgs/TransformStamped.h>
+#include <vector>
+
 // PCL specific includes
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
@@ -23,7 +43,8 @@
 static ros::Publisher PubOutput;
 static int ExampleNumber;
 
-void tf_broadcast(const std::string frame_id){
+void tf_broadcast(const std::string frame_id)
+{
     static tf2_ros::TransformBroadcaster br;
     geometry_msgs::TransformStamped transformStamped;
     transformStamped.header.stamp = ros::Time::now();
@@ -41,7 +62,8 @@ void tf_broadcast(const std::string frame_id){
     br.sendTransform(transformStamped);
 }
 
-void plane(const sensor_msgs::PointCloud2ConstPtr& cloud_msg, const std::string frame_id){
+void plane(const sensor_msgs::PointCloud2ConstPtr& cloud_msg, const std::string frame_id)
+{
     // PassThrough Filtering
     // Ref: http://www.pointclouds.org/documentation/tutorials/passthrough.php#passthrough
     // Ref: http://wiki.ros.org/perception_pcl/Tutorials
@@ -56,14 +78,14 @@ void plane(const sensor_msgs::PointCloud2ConstPtr& cloud_msg, const std::string 
     // Create the segmentation object
     pcl::SACSegmentation<pcl::PointXYZRGB> seg;
     // Optional
-    seg.setOptimizeCoefficients (true);
+    seg.setOptimizeCoefficients(true);
     // Mandatory
-    seg.setModelType (pcl::SACMODEL_PLANE);
-    seg.setMethodType (pcl::SAC_RANSAC);
-    seg.setDistanceThreshold (0.01);
-    
-    seg.setInputCloud (cloud.makeShared());
-    seg.segment (*inliers, *coefficients);
+    seg.setModelType(pcl::SACMODEL_PLANE);
+    seg.setMethodType(pcl::SAC_RANSAC);
+    seg.setDistanceThreshold(0.01);
+
+    seg.setInputCloud(cloud.makeShared());
+    seg.segment(*inliers, *coefficients);
 
     if (inliers->indices.size () == 0)
     {
@@ -71,7 +93,8 @@ void plane(const sensor_msgs::PointCloud2ConstPtr& cloud_msg, const std::string 
         exit(-1);
     }
 
-    for (size_t i = 0; i < inliers->indices.size (); ++i){
+    for (size_t i = 0; i < inliers->indices.size (); ++i)
+    {
         cloud.points[inliers->indices[i]].r = 255;
         cloud.points[inliers->indices[i]].g = 0;
         cloud.points[inliers->indices[i]].b = 0;
@@ -85,54 +108,57 @@ void plane(const sensor_msgs::PointCloud2ConstPtr& cloud_msg, const std::string 
     PubOutput.publish(output);
 }
 
-void euclideanClusterExtraction(const sensor_msgs::PointCloud2ConstPtr& cloud_msg, const std::string frame_id){
+void euclideanClusterExtraction(const sensor_msgs::PointCloud2ConstPtr& cloud_msg, const std::string frame_id)
+{
     // Euclidean Cluster Extraction
     // Ref: http://www.pointclouds.org/documentation/tutorials/cluster_extraction.php#cluster-extraction
-    
-    enum COLOR_RGB{
-        RED=0,
+
+    enum COLOR_RGB
+    {
+        RED = 0,
         GREEN,
         BLUE,
         COLOR_MAX
     };
     const int CLUSTER_MAX = 10;
-    const int CLUSTER_COLOR[CLUSTER_MAX][COLOR_MAX] = {
-        {230, 0, 18},{243, 152, 18}, {255, 251, 0},
-        {143, 195, 31},{0, 153, 68}, {0, 158, 150},
-        {0, 160, 233},{0, 104, 183}, {29, 32, 136},
+    const int CLUSTER_COLOR[CLUSTER_MAX][COLOR_MAX] =
+    {
+        {230, 0, 18}, {243, 152, 18}, {255, 251, 0},
+        {143, 195, 31}, {0, 153, 68}, {0, 158, 150},
+        {0, 160, 233}, {0, 104, 183}, {29, 32, 136},
         {146, 7, 131}
     };
 
     // Convert the sensor_msgs/PointCloud2 data to pcl/PointCloud
     // Use pcl::PointXYZRGB to visualize segmentation.
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>()); 
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>());
     pcl::fromROSMsg(*cloud_msg, *cloud);
 
     // Create the filtering object: downsample the dataset using a leaf size of 1cm
     pcl::VoxelGrid<pcl::PointXYZRGB> vg;
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZRGB>());
-    vg.setInputCloud (cloud);
-    vg.setLeafSize (0.01, 0.01, 0.01);
-    vg.filter (*cloud_filtered);
+    vg.setInputCloud(cloud);
+    vg.setLeafSize(0.01, 0.01, 0.01);
+    vg.filter(*cloud_filtered);
 
     // Create the segmentation object for the planar model and set all the parameters
     pcl::SACSegmentation<pcl::PointXYZRGB> seg;
     pcl::PointIndices::Ptr inliers (new pcl::PointIndices());
     pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients());
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_plane (new pcl::PointCloud<pcl::PointXYZRGB> ());
-    seg.setOptimizeCoefficients (true);
-    seg.setModelType (pcl::SACMODEL_PLANE);
-    seg.setMethodType (pcl::SAC_RANSAC);
-    seg.setMaxIterations (100);
-    seg.setDistanceThreshold (0.02);
+    seg.setOptimizeCoefficients(true);
+    seg.setModelType(pcl::SACMODEL_PLANE);
+    seg.setMethodType(pcl::SAC_RANSAC);
+    seg.setMaxIterations(100);
+    seg.setDistanceThreshold(0.02);
 
-    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_extracted(new pcl::PointCloud<pcl::PointXYZRGB>()); 
-    int i=0, nr_points = (int) cloud_filtered->points.size ();
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_extracted(new pcl::PointCloud<pcl::PointXYZRGB>());
+    int i = 0, nr_points = std::static_cast<int>(cloud_filtered->points.size());
     while (cloud_filtered->points.size () > 0.3 * nr_points)
     {
         // Segment the largest planar component from the remaining cloud
-        seg.setInputCloud (cloud_filtered);
-        seg.segment (*inliers, *coefficients);
+        seg.setInputCloud(cloud_filtered);
+        seg.segment(*inliers, *coefficients);
         if (inliers->indices.size () == 0)
         {
             ROS_WARN("Could not estimate a planar model for the given dataset.");
@@ -141,49 +167,51 @@ void euclideanClusterExtraction(const sensor_msgs::PointCloud2ConstPtr& cloud_ms
 
         // Extract the planar inliers from the input cloud
         pcl::ExtractIndices<pcl::PointXYZRGB> extract;
-        extract.setInputCloud (cloud_filtered);
-        extract.setIndices (inliers);
+        extract.setInputCloud(cloud_filtered);
+        extract.setIndices(inliers);
 
         // Remove the planar inliers, extract the rest
-        extract.setNegative (true);
-        extract.filter (*cloud_extracted);
+        extract.setNegative(true);
+        extract.filter(*cloud_extracted);
         *cloud_filtered = *cloud_extracted;
     }
 
     // Creating the KdTree object for the search method of the extraction
     pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGB>());
-    tree->setInputCloud (cloud_filtered);
+    tree->setInputCloud(cloud_filtered);
 
     std::vector<pcl::PointIndices> cluster_indices;
     pcl::EuclideanClusterExtraction<pcl::PointXYZRGB> ec;
-    ec.setClusterTolerance (0.02); // 2cm
-    ec.setMinClusterSize (100);
-    ec.setMaxClusterSize (25000);
-    ec.setSearchMethod (tree);
-    ec.setInputCloud (cloud_filtered);
-    ec.extract (cluster_indices);
+    ec.setClusterTolerance(0.02);  // 2cm
+    ec.setMinClusterSize(100);
+    ec.setMaxClusterSize(25000);
+    ec.setSearchMethod(tree);
+    ec.setInputCloud(cloud_filtered);
+    ec.extract(cluster_indices);
 
-    int cluster_i=0;
+    int cluster_i = 0;
     pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_output (new pcl::PointCloud<pcl::PointXYZRGB>());
-    for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin (); 
-            it != cluster_indices.end (); ++it)
+    for (std::vector<pcl::PointIndices>::const_iterator it = cluster_indices.begin ();
+         it != cluster_indices.end(); ++it)
     {
-        pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_cluster (new pcl::PointCloud<pcl::PointXYZRGB>());
-        for (std::vector<int>::const_iterator pit = it->indices.begin (); 
-                pit != it->indices.end (); ++pit){
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_cluster(new pcl::PointCloud<pcl::PointXYZRGB>());
+        for (std::vector<int>::const_iterator pit = it->indices.begin();
+                pit != it->indices.end(); ++pit)
+        {
             cloud_filtered->points[*pit].r = CLUSTER_COLOR[cluster_i][RED];
             cloud_filtered->points[*pit].g = CLUSTER_COLOR[cluster_i][GREEN];
             cloud_filtered->points[*pit].b = CLUSTER_COLOR[cluster_i][BLUE];
-            cloud_cluster->points.push_back (cloud_filtered->points[*pit]); //*
+            cloud_cluster->points.push_back(cloud_filtered->points[*pit]);
         }
-        cloud_cluster->width = cloud_cluster->points.size ();
+        cloud_cluster->width = cloud_cluster->points.size();
         cloud_cluster->height = 1;
         cloud_cluster->is_dense = true;
-        
+
         *cloud_output += *cloud_cluster;
 
         cluster_i++;
-        if(cluster_i >= CLUSTER_MAX){
+        if (cluster_i >= CLUSTER_MAX)
+        {
             break;
         }
     }
@@ -198,11 +226,12 @@ void euclideanClusterExtraction(const sensor_msgs::PointCloud2ConstPtr& cloud_ms
 }
 
 
-void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
+void cloud_cb(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
 {
-    const static std::string EXAMPLE_FRAME_ID = "example_frame";
+    const std::string EXAMPLE_FRAME_ID = "example_frame";
 
-    switch(ExampleNumber){
+    switch (ExampleNumber)
+    {
     case 0:
         plane(cloud_msg, EXAMPLE_FRAME_ID);
         break;
@@ -219,10 +248,10 @@ void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
     tf_broadcast(EXAMPLE_FRAME_ID);
 }
 
-int main (int argc, char** argv)
+int main(int argc, char** argv)
 {
     // Initialize ROS
-    ros::init (argc, argv, "example_segmentation");
+    ros::init(argc, argv, "example_segmentation");
     ros::NodeHandle nh("~");
 
     nh.param<int>("number", ExampleNumber, 0);
@@ -233,5 +262,5 @@ int main (int argc, char** argv)
     // Create a ROS publisher for the output point cloud
     PubOutput = nh.advertise<sensor_msgs::PointCloud2> ("/output", 1);
 
-    ros::spin ();
+    ros::spin();
 }
